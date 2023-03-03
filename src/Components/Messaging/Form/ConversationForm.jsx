8a9a9883d0a9b48e62c2a1dda7b955d9b4625fa1
Rares <raresmusea001@gmail.com>
2023-03-04 00:45:01 +0200

    Finished the design for the message sender form. A new design for the messages still need to be implemented.

--- src/Components/Messaging/Form/ConversationForm.jsx
+++ src/Components/Messaging/Form/ConversationForm.jsx
@@ -1,7 +1,24 @@
 import '../../../Styles/Messaging/Form/ConversationForm.scss';
+import IconButton from "@mui/joy/IconButton";
+import {AttachFile, MoreHorizOutlined, Send} from "@mui/icons-material";
 
 export default function ConversationForm() {
     return (
-        <div>ConversationForm</div>
+        <div className="ConversationFormWrapper">
+            <textarea className="MessageInput"
+                      placeholder="Message..."/>
+            <IconButton title="Send message"
+                        className="MessageSenderIconButton">
+                <Send className="SendIcon"/>
+            </IconButton>
+            <IconButton title="Attach file"
+                        className="MessageSenderIconButton">
+                <AttachFile className="AttachIcon"/>
+            </IconButton>
+            <IconButton title="More"
+                        className="MessageSenderIconButton">
+                <MoreHorizOutlined className="MoreOptionsIcon"/>
+            </IconButton>
+        </div>
     )
 }
